77d0ef29abb968e5786565a5cd459e1d4a6b5f6a
Rafael Tessarolo <rafinha.tessarolo@hotmail.com>
2020-08-09 15:11:57 -0300

    Converted application to be mobile friendly

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,9 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
-
 import Header from './components/header/header.component';
+
+import { GlobalStyle } from './global.styles';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -50,6 +50,7 @@ const App = ({ setCurrentUser, currentUser }) => {
 
   return (
     <div>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
